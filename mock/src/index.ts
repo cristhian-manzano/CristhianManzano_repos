f3e6cd3c8391559616dcfaf6eb97f8fafd8f3941
Cristhian Manzano <crisven1998@gmail.com>
2022-07-03 15:11:41 -0500

    feat(mock): added express server

--- mock/src/index.ts
+++ mock/src/index.ts
@@ -1,1 +1,14 @@
-console.log('Hello world');
+import * as dotenv from 'dotenv';
+import express from 'express';
+
+dotenv.config();
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+const PORT = process.env.APP_PORT ?? 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running in port ${PORT}`);
+});
