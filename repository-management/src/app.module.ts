e88c9d5e099a247c3447443f0ee82946b5b8862e
Cristhian Manzano <crisven1998@gmail.com>
2022-07-03 18:37:04 -0500

    feat(metrics): create metrics module

--- repository-management/src/app.module.ts
+++ repository-management/src/app.module.ts
@@ -3,12 +3,14 @@ import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TypeOrmConfigService } from './config/typeorm.service';
 import { OrganizationManagementModule } from './organizationManagement/organizationManagement.module';
+import { MetricsModule } from './metrics/metrics.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     TypeOrmModule.forRootAsync({ useClass: TypeOrmConfigService }),
     OrganizationManagementModule,
+    MetricsModule,
   ],
   controllers: [],
   providers: [],
