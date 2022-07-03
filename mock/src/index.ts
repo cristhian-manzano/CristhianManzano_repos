5a112c2b5757ee250235e879ed59184a2a288d05
Cristhian Manzano <crisven1998@gmail.com>
2022-07-03 15:15:20 -0500

    feat(mock): added general middleware configurations express

--- mock/src/index.ts
+++ mock/src/index.ts
@@ -1,11 +1,20 @@
 import * as dotenv from 'dotenv';
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
+import helmet from 'helmet';
+import cors from 'cors';
 
 dotenv.config();
 const app = express();
 
+app.use(helmet());
+app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.use((err: Error, _req: Request, res: Response, _next: NextFunction) => {
+  console.log('Error: ', err);
+  return res.status(500).send('Internal server error!');
+});
 
 const PORT = process.env.APP_PORT ?? 3000;
 
