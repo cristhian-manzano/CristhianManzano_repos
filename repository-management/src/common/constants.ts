4f5ce28dc116b24b102a04588b71128aa3c424f3
Cristhian Manzano <crisven1998@gmail.com>
2022-07-03 21:05:36 -0500

    feat(metrics): add constants file

---
+++ repository-management/src/common/constants.ts
@@ -0,0 +1,19 @@
+export const REPOSITORY = {
+  VERIFICATION_STATE: {
+    604: 'Verificado',
+    605: 'En espera',
+    606: 'Aprobado',
+  },
+
+  STATE: {
+    E: 'Habilitado',
+    D: 'Deshabilitado',
+    A: 'Archivado',
+  },
+
+  STATUS: {
+    E: 'Habilitado',
+    D: 'Deshabilitado',
+    A: 'Archivado',
+  },
+};
